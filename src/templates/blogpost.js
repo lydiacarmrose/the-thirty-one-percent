f0c302b6b4fecaddf7ecbeb73cb344e8b37d7ef7
lydiacarmrose <61116085+lydiacarmrose@users.noreply.github.com>
2020-04-10 15:02:55 -0400

    updates 4.10.20

--- src/templates/blogpost.js
+++ src/templates/blogpost.js
@@ -137,7 +137,106 @@ function prepContent(dataContent) {
 
   return nodes.join("")
 }
+// function prepContentintro(dataContent) {
+//   const useContent = []
 
+//   var firsth2 = true
+//   for (const [index, value] of dataContent.entries()) {
+//     if (value.content.length > 0) {
+//       const nodeType = value.nodeType
+//       const content = value.content[0].value
+
+//       const obj = {
+//         [KEY_CONTENT]: content,
+//         [KEY_NODE_TYPE]: nodeType,
+//       }
+
+//       if (nodeType == "heading-2" && firsth2) {
+//         obj[KEY_FIRST_H2] = true
+//         firsth2 = false
+//       }
+
+//       useContent.push(obj)
+//     }
+//   }
+
+//   var nextParaIsLastPara = false
+//   for (var i = useContent.length - 1; i >= 0; i--) {
+//     const content = useContent[i]
+//     const nodeType = content[KEY_NODE_TYPE]
+//     if (nodeType == "heading-2") {
+//       nextParaIsLastPara = true
+//     } else if (nodeType == "paragraph") {
+//       content[KEY_LAST_PARA] = nextParaIsLastPara
+//       nextParaIsLastPara = false
+//     }
+//   }
+
+//   var leftWinder = true
+//   for (const content of useContent) {
+//     if (content[KEY_NODE_TYPE] == "paragraph" && content[KEY_LAST_PARA]) {
+//       content[KEY_LEFT_WINDER] = leftWinder
+//       leftWinder = !leftWinder
+//     }
+//   }
+
+//   const lastParaData = {
+//     [KEY_PARAS_IN_SECTION]: [],
+//     [KEY_LEFT_WINDER]: false,
+//   }
+
+//   for (const d of useContent) {
+//     const content = d[KEY_CONTENT]
+//     const nodeType = d[KEY_NODE_TYPE]
+//     if (nodeType == "heading-2") {
+//       wrapParas(
+//         lastParaData[KEY_PARAS_IN_SECTION],
+//         lastParaData[KEY_LEFT_WINDER],
+//         nodes
+//       )
+//       lastParaData[KEY_PARAS_IN_SECTION].length = 0
+
+//       const h2 = `
+//       <div class="w-full m-auto">
+//         <h2
+//           style="font-family: 'Copse';"
+//           class="m-auto text-3xl text-blue-700 text-center"
+//         >
+//           ${content}
+//         </h2>
+//       </div>`
+
+//       nodes.push(h2)
+//     } else if (nodeType == "paragraph") {
+//       const pClass = "bg-yellow-700"
+//       const pClassLastLeft =
+//         "bg-yellow-700"
+//       const pClassLastRite =
+//         "bg-yellow-700"
+//       const pClassOption = d[KEY_LAST_PARA]
+//         ? d[KEY_LEFT_WINDER]
+//           ? pClassLastLeft
+//           : pClassLastRite
+//         : pClass
+
+//       const p = `
+//           <p
+//             class="${pClassOption}"
+//           >
+//             ${content}
+//           </p>
+//       `
+
+//       lastParaData[KEY_PARAS_IN_SECTION].push(p)
+//       lastParaData[KEY_LEFT_WINDER] = d[KEY_LEFT_WINDER]
+//     }
+//   }
+
+//   wrapParas(lastParaData[KEY_PARAS_IN_SECTION], null, nodes)
+//   lastParaData[KEY_PARAS_IN_SECTION].length = 0
+
+//   return nodes.join("")
+// }
 
 const Article = ({ data }) => {
   const {
@@ -150,7 +249,7 @@ const Article = ({ data }) => {
     mainImage,
   } = data.contentfulArticle
   const parsedBodyText = prepContent(bodyText.json.content)
-  // const parsedIntro = prepContent(introText.json.content)
+  // const parsedIntro = prepContentintro(introText.json.content)
 
   return (
     <Layout>
@@ -163,7 +262,7 @@ const Article = ({ data }) => {
           />
         </div>
         <div className="lg:col-start-2 lg:col-span-1 lg:row-span-1s">
-          <h1 className="ml-2 lg:-mt-0 lg:ml-0 text-6xl leading-tight lg:-ml-2 lg:pt-32 ">
+          <h1 className="text-white ml-2 lg:-mt-0 lg:ml-0 text-6xl leading-tight lg:-ml-2 lg:pt-32 ">
             {title}
             <br /> {lastName}
           </h1>
@@ -172,12 +271,19 @@ const Article = ({ data }) => {
           </h2>
           <h3>{featureType2.title}</h3>
         </div>
-        <div className="mt-5 mx-auto ml-8 mr-8 border-l-2 border-blue-700 border-t-2 lg:border-l-4 lg:border-t-4 lg:col-start-2 lg:row-start-2 lg:row-span-1 lg:col-span-1 pt-2 lg:pt-4 pl-2 lg:pl-4 lg:mr-48 lg:ml-16">
+        
+    <div className="mt-5 mx-auto ml-8 mr-8 border-l-2 border-blue-700 border-t-2 lg:border-l-4 lg:border-t-4 lg:col-start-2 lg:row-start-2 lg:row-span-1 lg:col-span-1 pt-2 lg:pt-4 pl-2 lg:pl-4 lg:mr-48 lg:ml-16">
           <p className="font-serif">
-            {/* <em>{ReactHtmlParser(parsedIntro)}</em> */}
+            <em>Dummy Text</em>
           </p>
-        </div>
+   </div>
       </div>
+      <div class="hidden lg:visible lg:grid lg:grid-cols-2 ">
+    <div class="lg:col-span-1 lg:px-1 ">
+    </div>
+    <div class="h-0 lg:border-l-4 lg:border-blue-700 lg:col-start-2 lg:h-20 lg:col-span-1 lg:pt-4 lg:pl-4 lg:pr-32 lg:ml-16">
+    </div>
+  </div>
 
       <div> {ReactHtmlParser(parsedBodyText)} </div>
 
@@ -218,3 +324,4 @@ export const query = graphql`
     }
   }
 `
+
