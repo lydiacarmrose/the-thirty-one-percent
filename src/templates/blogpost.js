2cc5298a611f659b47e29771a05eab46d57e7ae9
Ricardo Villamil <ricardovj1@gmail.com>
2020-04-11 12:00:45 -0400

    Fix intro text, styling and duplicates in home page

--- src/templates/blogpost.js
+++ src/templates/blogpost.js
@@ -38,6 +38,7 @@ function wrapParas(parasInSection, left, nodes) {
 }
 
 function prepContent(dataContent) {
+  const nodes = []
   const useContent = []
 
   var firsth2 = true
@@ -96,7 +97,19 @@ function prepContent(dataContent) {
       )
       lastParaData[KEY_PARAS_IN_SECTION].length = 0
 
-      const h2 = `
+      let h2
+      if (d[KEY_FIRST_H2]) {
+        h2 = `
+        <div class="w-full m-auto">
+          <h2
+            style="font-family: 'Copse';"
+            class="bg-yellow-700"
+          >
+            ${content}
+          </h2>
+        </div>`
+      } else {
+        h2 = `
       <div class="w-full m-auto">
         <h2
           style="font-family: 'Copse';"
@@ -105,6 +118,7 @@ function prepContent(dataContent) {
           ${content}
         </h2>
       </div>`
+      }
 
       nodes.push(h2)
     } else if (nodeType == "paragraph") {
@@ -137,106 +151,94 @@ function prepContent(dataContent) {
 
   return nodes.join("")
 }
-// function prepContentintro(dataContent) {
-//   const useContent = []
 
-//   var firsth2 = true
-//   for (const [index, value] of dataContent.entries()) {
-//     if (value.content.length > 0) {
-//       const nodeType = value.nodeType
-//       const content = value.content[0].value
+function prepContentintro(dataContent) {
+  const nodes = []
+  const useContent = []
 
-//       const obj = {
-//         [KEY_CONTENT]: content,
-//         [KEY_NODE_TYPE]: nodeType,
-//       }
+  var firsth2 = true
+  for (const [index, value] of dataContent.entries()) {
+    if (value.content.length > 0) {
+      const nodeType = value.nodeType
+      const content = value.content[0].value
 
-//       if (nodeType == "heading-2" && firsth2) {
-//         obj[KEY_FIRST_H2] = true
-//         firsth2 = false
-//       }
+      const obj = {
+        [KEY_CONTENT]: content,
+        [KEY_NODE_TYPE]: nodeType,
+      }
 
-//       useContent.push(obj)
-//     }
-//   }
+      if (nodeType == "heading-2" && firsth2) {
+        obj[KEY_FIRST_H2] = true
+        firsth2 = false
+      }
 
-//   var nextParaIsLastPara = false
-//   for (var i = useContent.length - 1; i >= 0; i--) {
-//     const content = useContent[i]
-//     const nodeType = content[KEY_NODE_TYPE]
-//     if (nodeType == "heading-2") {
-//       nextParaIsLastPara = true
-//     } else if (nodeType == "paragraph") {
-//       content[KEY_LAST_PARA] = nextParaIsLastPara
-//       nextParaIsLastPara = false
-//     }
-//   }
+      useContent.push(obj)
+    }
+  }
 
-//   var leftWinder = true
-//   for (const content of useContent) {
-//     if (content[KEY_NODE_TYPE] == "paragraph" && content[KEY_LAST_PARA]) {
-//       content[KEY_LEFT_WINDER] = leftWinder
-//       leftWinder = !leftWinder
-//     }
-//   }
+  var nextParaIsLastPara = false
+  for (var i = useContent.length - 1; i >= 0; i--) {
+    const content = useContent[i]
+    const nodeType = content[KEY_NODE_TYPE]
+    if (nodeType == "heading-2") {
+      nextParaIsLastPara = true
+    } else if (nodeType == "paragraph") {
+      content[KEY_LAST_PARA] = nextParaIsLastPara
+      nextParaIsLastPara = false
+    }
+  }
 
-//   const lastParaData = {
-//     [KEY_PARAS_IN_SECTION]: [],
-//     [KEY_LEFT_WINDER]: false,
-//   }
+  var leftWinder = true
+  for (const content of useContent) {
+    if (content[KEY_NODE_TYPE] == "paragraph" && content[KEY_LAST_PARA]) {
+      content[KEY_LEFT_WINDER] = leftWinder
+      leftWinder = !leftWinder
+    }
+  }
 
-//   for (const d of useContent) {
-//     const content = d[KEY_CONTENT]
-//     const nodeType = d[KEY_NODE_TYPE]
-//     if (nodeType == "heading-2") {
-//       wrapParas(
-//         lastParaData[KEY_PARAS_IN_SECTION],
-//         lastParaData[KEY_LEFT_WINDER],
-//         nodes
-//       )
-//       lastParaData[KEY_PARAS_IN_SECTION].length = 0
+  const lastParaData = {
+    [KEY_PARAS_IN_SECTION]: [],
+    [KEY_LEFT_WINDER]: false,
+  }
 
-//       const h2 = `
-//       <div class="w-full m-auto">
-//         <h2
-//           style="font-family: 'Copse';"
-//           class="m-auto text-3xl text-blue-700 text-center"
-//         >
-//           ${content}
-//         </h2>
-//       </div>`
+  for (const d of useContent) {
+    const content = d[KEY_CONTENT]
+    const nodeType = d[KEY_NODE_TYPE]
+    if (nodeType == "heading-2") {
+      wrapParas(
+        lastParaData[KEY_PARAS_IN_SECTION],
+        lastParaData[KEY_LEFT_WINDER],
+        nodes
+      )
+      lastParaData[KEY_PARAS_IN_SECTION].length = 0
+    } else if (nodeType == "paragraph") {
+      const pClass = "bg-yellow-700"
+      const pClassLastLeft = "bg-yellow-700"
+      const pClassLastRite = "bg-yellow-700"
+      const pClassOption = d[KEY_LAST_PARA]
+        ? d[KEY_LEFT_WINDER]
+          ? pClassLastLeft
+          : pClassLastRite
+        : pClass
 
-//       nodes.push(h2)
-//     } else if (nodeType == "paragraph") {
-//       const pClass = "bg-yellow-700"
-//       const pClassLastLeft =
-//         "bg-yellow-700"
-//       const pClassLastRite =
-//         "bg-yellow-700"
-//       const pClassOption = d[KEY_LAST_PARA]
-//         ? d[KEY_LEFT_WINDER]
-//           ? pClassLastLeft
-//           : pClassLastRite
-//         : pClass
+      const p = `
+          <p
+            class="${pClassOption}"
+          >
+            ${content}
+          </p>
+      `
 
-//       const p = `
-//           <p
-//             class="${pClassOption}"
-//           >
-//             ${content}
-//           </p>
-//       `
+      lastParaData[KEY_PARAS_IN_SECTION].push(p)
+      lastParaData[KEY_LEFT_WINDER] = d[KEY_LEFT_WINDER]
+    }
+  }
 
-//       lastParaData[KEY_PARAS_IN_SECTION].push(p)
-//       lastParaData[KEY_LEFT_WINDER] = d[KEY_LEFT_WINDER]
-//     }
-//   }
+  wrapParas(lastParaData[KEY_PARAS_IN_SECTION], null, nodes)
+  lastParaData[KEY_PARAS_IN_SECTION].length = 0
 
-//   wrapParas(lastParaData[KEY_PARAS_IN_SECTION], null, nodes)
-//   lastParaData[KEY_PARAS_IN_SECTION].length = 0
-
-//   return nodes.join("")
-// }
+  return nodes.join("")
+}
 
 const Article = ({ data }) => {
   const {
@@ -249,7 +251,7 @@ const Article = ({ data }) => {
     mainImage,
   } = data.contentfulArticle
   const parsedBodyText = prepContent(bodyText.json.content)
-  // const parsedIntro = prepContentintro(introText.json.content)
+  const parsedIntro = prepContentintro(introText.json.content)
 
   return (
     <Layout>
@@ -271,19 +273,18 @@ const Article = ({ data }) => {
           </h2>
           <h3>{featureType2.title}</h3>
         </div>
-        
-    <div className="mt-5 mx-auto ml-8 mr-8 border-l-2 border-blue-700 border-t-2 lg:border-l-4 lg:border-t-4 lg:col-start-2 lg:row-start-2 lg:row-span-1 lg:col-span-1 pt-2 lg:pt-4 pl-2 lg:pl-4 lg:mr-48 lg:ml-16">
+
+        <div className="mt-5 mx-auto ml-8 mr-8 border-l-2 border-blue-700 border-t-2 lg:border-l-4 lg:border-t-4 lg:col-start-2 lg:row-start-2 lg:row-span-1 lg:col-span-1 pt-2 lg:pt-4 pl-2 lg:pl-4 lg:mr-48 lg:ml-16">
+          <div>{ReactHtmlParser(parsedIntro)}</div>
           <p className="font-serif">
-            <em>Dummy Text</em>
+            <em>Dummy text</em>
           </p>
-   </div>
+        </div>
       </div>
-      <div class="hidden lg:visible lg:grid lg:grid-cols-2 ">
-    <div class="lg:col-span-1 lg:px-1 ">
-    </div>
-    <div class="h-0 lg:border-l-4 lg:border-blue-700 lg:col-start-2 lg:h-20 lg:col-span-1 lg:pt-4 lg:pl-4 lg:pr-32 lg:ml-16">
-    </div>
-  </div>
+      <div className="hidden lg:visible lg:grid lg:grid-cols-2 ">
+        <div className="lg:col-span-1 lg:px-1 "></div>
+        <div className="h-0 lg:border-l-4 lg:border-blue-700 lg:col-start-2 lg:h-20 lg:col-span-1 lg:pt-4 lg:pl-4 lg:pr-32 lg:ml-16"></div>
+      </div>
 
       <div> {ReactHtmlParser(parsedBodyText)} </div>
 
@@ -292,8 +293,6 @@ const Article = ({ data }) => {
     </Layout>
   )
 }
-
-const nodes = []
 
 export default Article
 export const query = graphql`
@@ -324,4 +323,3 @@ export const query = graphql`
     }
   }
 `
-
