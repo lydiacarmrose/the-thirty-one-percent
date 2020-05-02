335bc3d6927e198c2a12a3503dc730b401eee555
lydiacarmrose <61116085+lydiacarmrose@users.noreply.github.com>
2020-05-02 18:25:37 -0400

    switch to local fonts for speed

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
+       
         fonts: [
-          `Signika\:300,400,600.700`,
+          `limelight`,
           `source sans pro\:300,400,400i,700`, // you can also specify font weights and styles
-          `Shadows Into Light`,
+          
+
         ],
         display: 'swap'
       }
